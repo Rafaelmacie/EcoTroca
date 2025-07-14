ede23947c86f2baf9dd26f6742385272b3d5460f
KevenGonCabral <profissional172@gmail.com>
2025-07-14 20:34:47 -0300

    adicionando o footer, e estilizando o footer

--- src/pages/TodosProdutos.jsx
+++ src/pages/TodosProdutos.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ListaProdutos from '../components/ListaProdutos/ListaProdutos'
+import Footer from '../components/Footer/Footer'
 
 function TodosProdutos() {
   return (
     <>
         <ListaProdutos />
+        <Footer />
     </>
     
   )
 }
 
-export default TodosProdutos
+export default TodosProdutos;
