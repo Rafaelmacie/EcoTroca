49eba3d6adfe97cfee5fb06f406505d568c4f6fb
Rafael Maciel <rafaelmn934@gmail.com>
2025-07-15 10:02:00 -0300

    Adicionando texto de boas vindas à página Home

---
+++ src/components/BoasVindas/BoasVindas.jsx
@@ -0,0 +1,12 @@
+function BoasVindas(){
+    return(
+        <section>
+            <h2>Seja bem-vindo à EcoTroca!</h2>
+            <p>
+                Aqui no nosso sistema você pode encontrar itens de seu interesse e propor trocas por seus próprios produtos. Lembre-se: Aquilo que você não tem mais necessidade, pode ter um valor imensurável para alguém que precise.
+            </p>
+        </section>
+    );
+}
+
+export default BoasVindas;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import CustomCarousel from "../components/Carousel/Carousel";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from "../components/Footer/Footer";
+import BoasVindas from "../components/BoasVindas/BoasVindas";
 
 function Home() {
   return (
     <>
+      <BoasVindas />
       <CustomCarousel />
       <Footer />
   </>
