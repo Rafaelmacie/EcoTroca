cac4371b1c5077fdecc13b75649b5047f7942d7b
Rafael Maciel <rafaelmn934@gmail.com>
2025-07-15 10:32:48 -0300

    Estilizando texto de Boas vindas

--- src/components/BoasVindas/BoasVindas.jsx
+++ src/components/BoasVindas/BoasVindas.jsx
@@ -1,9 +1,13 @@
+import styles from './BoasVindas.module.css';
+
 function BoasVindas(){
     return(
-        <section>
-            <h2>Seja bem-vindo à EcoTroca!</h2>
-            <p>
-                Aqui no nosso sistema você pode encontrar itens de seu interesse e propor trocas por seus próprios produtos. Lembre-se: Aquilo que você não tem mais necessidade, pode ter um valor imensurável para alguém que precise.
+        <section className="container my-4">
+            <h2 className={styles.titulo}>Seja bem-vindo à <span className={styles.EcoTroca}>EcoTroca!</span></h2>
+            <p className={styles.paragrafo}>
+                Aqui no nosso sistema você pode encontrar itens de seu interesse e propor trocas por seus próprios produtos. 
+                <br/>
+                <b>Lembre-se: Aquilo que você não tem mais necessidade, pode ter um valor imensurável para alguém que precise.</b>
             </p>
         </section>
     );
